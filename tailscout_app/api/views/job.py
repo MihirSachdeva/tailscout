066024769fbb3c7e2eca4332a382a53c5a4decff
igembtiitr <64272723+igembtiitr@users.noreply.github.com>
2020-10-22 02:10:54 +0530

    Update job.py
    updated.

--- tailscout_app/api/views/job.py
+++ tailscout_app/api/views/job.py
@@ -22,36 +22,40 @@ class JobViewSet(viewsets.ModelViewSet):
         new_job_email_id = new_job.email_id
 
         # Directory
-        directory = str(new_job_id)
-
+        directory = str(f"{new_job_id}_folder")
+        current_directory = os.getcwd()
         # Parent Directory path
-        parent_dir = "/home/pradum/PycharmProjects/djangoProject/tailscout-master/tailscout_app/"
+        parent_dir = f"{current_directory}/tailscout_app/"
 
         # Path
         path = os.path.join(parent_dir, directory)
-        print(os.getcwd())
+
         try:
             os.makedirs(path, exist_ok=True)
             print("Directory '%s' created successfully" % directory)
         except OSError as error:
             print("Directory '%s' can not be created" % directory)
 
-        print(os.getcwd())
-        os.chdir("/home/pradum/PycharmProjects/djangoProject/tailscout-master/tailscout_app/")
 
-        print(os.getcwd())
+        os.chdir(f"{current_directory}/tailscout_app/")
+
+
 
         files = ['genee.py', 'clustalo.py', 'phage_details.csv', 'jpred.pl','sequence_phages.fasta']
         for f in files:
-            shutil.copy(f, f"{new_job_id}")
-        print(os.getcwd())
+            shutil.copy(f, f"{new_job_id}_folder")
+
 
         dname = os.path.dirname(__file__)
-        fname1 = os.path.join(dname, f'../../{new_job_id}/genee.py')
-        fname2 = os.path.join(dname, f'../../{new_job_id}/clustalo.py')
-        fname3 = os.path.join(dname, f'../../{new_job_id}/jpred.pl')
+        fname1 = os.path.join(dname, f'../../{new_job_id}_folder/genee.py')
+        fname2 = os.path.join(dname, f'../../{new_job_id}_folder/clustalo.py')
+        fname3 = os.path.join(dname, f'../../{new_job_id}_folder/jpred.pl')
 
-        os.system(f"python {fname1} --bacteria {new_job_bacteria} --filename {new_job_id}")
+        os.chdir(f"{current_directory}/tailscout_app/{new_job_id}_folder")
+
+        print(os.getcwd())
+
+        os.system(f"python {fname1} --bacteria '{new_job_bacteria}' --filename {new_job_id}")
         # new_job_id.fasta created
         new_job.status = "S1"
         print("Step 1 done.")
@@ -59,23 +63,23 @@ class JobViewSet(viewsets.ModelViewSet):
 
         files = [f'{new_job_id}.fasta']
         for f in files:
-            shutil.copy(f, f"{new_job_id}")
+            shutil.copy(f, f"{new_job_id}_folder")
 
-        os.system(f"python {fname2} --email {new_job_email_id} --sequence {new_job_id}")
+        os.system(f"python {fname2} --email {new_job_email_id} --sequence {new_job_id}.fasta")
         # new_job_id_ABC.fasta created
         new_job.status = "S2"
         print("Step 2 done!")
         new_job.save()
 
-        if bacteria == "acinetobacter":
+        if bacteria == "acinetobacter baumannii":
             file = open(f"{new_job_id}a.fasta", "w+")
-            L = [">MTTNTPKYGGLLTDIGAAALATASAAGKKWQPTHMLIGDAGGAPGDTPDPLPSAA" + "\n" + "QKSLINQRHRAQLNRLFVSDKNANTLVAEVVLPVEVGGFWIREIGLQDADGKFVAVSNCP" + "\n" + "PSYKAAMESGSARTQTIRVNIALSGLENVQLLIDNGIIYATQDWVKEKVAADFKGRKILAGNGLLG" + "\n" + "GGDLSADRSIGLAPSGVTAGSYRSVTVNANGVVTQGSNPTTLAGYAIGDAYTKADTDGKLAQKANKATTL" + "\n" +  "AGYGITDALRVDGNAVSSSRLAAPRSLAASGDASWSVTFDGSANVSAPLSLSATGVAAGSYPKVTVDTKGRVTA" + "\n" + "GMALAATDIPGLDASKLVSGVLAEQRLPVFARGLATAVSNSSDPNTATVPLMLTNHANGPVAGRYFYIQS" + "\n" + "MFYPDQNGNASQIATSYNATSEMYVRVSYAANPSIREWLPWQRCDIGGSFTKEADGELPGGVNLDSMVTSG" + "\n" + "WWSQSFTAQAASGANYPIVRAGLLHVYAASSNFIYQTYQAYDGESFYFRCRHSNTWFPWRRMWHGGDFNPSDYL" + "\n" + "LKSGFYWNALPGKPATFIPTATSTTAGITKVLNVLNSNDVGSALSAAQGKVLNDKFNFQNSKNQSGYVRLGDSGLIIQ" + "\n" + "WGVFTSTKTQSNLIFPLAFPNALLSITGNLNSNTPDVIGIDFDLSTATKTSIKTGAAQVGASWLSGKKISWIAIGY" ]
+            L = [">MTTNTPKYGGLLTDIGAAALATASAAGKKWQPTHMLIGDAGGAPGDTPDPLPSAAQKSLI" + "\n" + "NQRHRAQLNRLFVSDKNANTLVAEVVLPVEVGGFWIREIGLQDADGKFVAVSNCPPSYKA" + "\n" + "AMESGSARTQTIRVNIALSGLENVQLLIDNGIIYATQDWVKEKVAADFKGRKILAGNGLL" + "\n" + "GGGDLSADRSIGLAPSGVTAGSYRSVTVNANGVVTQGSNPTTLAGYAIGDAYTKADTDGK" + "\n" +  "LAQKANKATTLAGYGITDALRVDGNAVSSSRLAAPRSLAASGDASWSVTFDGSANVSAPL" + "\n" + "SLSATGVAAGSYPKVTVDTKGRVTAGMALAATDIPGLDASKLVSGVLAEQRLPVFARGLA" + "\n" + "TAVSNSSDPNTATVPLMLTNHANGPVAGRYFYIQSMFYPDQNGNASQIATSYNATSEMYV" + "\n" + "RVSYAANPSIREWLPWQRCDIGGSFTKEADGELPGGVNLDSMVTSGWWSQSFTAQAASGA" + "\n" + "NYPIVRAGLLHVYAASSNFIYQTYQAYDGESFYFRCRHSNTWFPWRRMWHGGDFNPSDYL" + "\n" + "LKSGFYWNALPGKPATFIPTATSTTAGITKVLNVLNSNDVGSALSAAQGKVLNDKFNFQN" + "\n" +  "SKNQSGYVRLGDSGLIIQWGVFTSTKTQSNLIFPLAFPNALLSITGNLNSNTPDVIGIDF" + "\n" + "DLSTATKTSIKTGAAQVGASWLSGKKISWIAIGY"]
             file.writelines(L)
             file.close()
 
         files = [f'{new_job_id}a.fasta']
         for f in files:
-            shutil.copy(f, f"{new_job_id}")
+            shutil.copy(f, f"{new_job_id}_folder")
 
         os.system(f"perl {fname3} submit file={new_job_id}a.fasta mode=batch format=fasta email={new_job_email_id} ")
         new_job.status = "S3"
